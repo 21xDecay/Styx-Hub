d6d4c2ef6593deadc7e77b045b9ac1287d7d7429
21xDecay <128763846+21xDecay@users.noreply.github.com>
2025-01-07 17:14:48 -0500

    Delete loader.lua

--- loader.lua
+++
@@ -1,5 +0,0 @@
-if game.PlaceId == 4588604953 or game.PlaceId == 15169310267 or game.PlaceId == 8343259840 or game.PlaceId == 15169306359 or game.PlaceId == 15169303036 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/21xDecay/Styx-Hub/refs/heads/main/criminality.lua"))()
-else
-    print("Game Not Supported!")
-end
