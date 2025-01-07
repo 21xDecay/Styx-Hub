6e71ffb9dab0299d588113610f3d1e5f0a906519
21xDecay <128763846+21xDecay@users.noreply.github.com>
2025-01-07 09:11:01 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,6 @@
-
+if game.PlaceId == 4588604953 or game.PlaceId == 15169310267 or game.PlaceId == 8343259840 or game.PlaceId == 15169306359 or game.PlaceId == 15169303036 then
+    -- loadstring(game:HttpGet("link"))()
+  print("script loaded")
+else
+    print("Game Not Supported!")
+end
